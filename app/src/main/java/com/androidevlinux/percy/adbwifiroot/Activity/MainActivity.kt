2b70c5d5a1200cc8a8d0291d7b976c64d586b6a5
percy-g2 <gahlotpercy@gmail.com>
2017-11-04 00:55:58 +0530

    Check for Wifi connected or not. Start preparing for About fragment.

--- app/src/main/java/com/androidevlinux/percy/adbwifiroot/Activity/MainActivity.kt
+++ app/src/main/java/com/androidevlinux/percy/adbwifiroot/Activity/MainActivity.kt
@@ -5,14 +5,18 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.androidevlinux.percy.adbwifiroot.Fragment.About
+import com.androidevlinux.percy.adbwifiroot.Fragment.Settings
 import com.androidevlinux.percy.adbwifiroot.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -21,10 +25,8 @@ import java.io.IOException
 import java.io.OutputStreamWriter
 
 
-
-
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
+    private var fragment: Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,14 +40,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         nav_view.itemIconTintList = null
 
         turn_on_adb_wifi.setOnCheckedChangeListener{ _, isChecked ->
-            if (isChecked) {
-                enableWifiAdb(isChecked)
-                changeState()
-                Snackbar.make(turn_on_adb_wifi, "ADB WiFi Turned On", Snackbar.LENGTH_SHORT).show()
-            } else {
-                enableWifiAdb(isChecked)
-                changeState()
-                Snackbar.make(turn_on_adb_wifi, "ADB WiFi Turned Off", Snackbar.LENGTH_SHORT).show()
+            if (checkWiFi()) {
+                if (isChecked) {
+                    enableWifiAdb(isChecked)
+                    changeState()
+                    Snackbar.make(turn_on_adb_wifi, "ADB WiFi Turned On", Snackbar.LENGTH_SHORT).show()
+                } else {
+                    enableWifiAdb(isChecked)
+                    changeState()
+                    Snackbar.make(turn_on_adb_wifi, "ADB WiFi Turned Off", Snackbar.LENGTH_SHORT).show()
+                }
+            } else{
+                turn_on_adb_wifi.isChecked = false
+                Snackbar.make(turn_on_adb_wifi, "Connect To WiFi First", Snackbar.LENGTH_SHORT).show()
             }
         }
         changeState()
@@ -61,6 +68,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
+        displaySelectedScreen(item.itemId)
         when (item.itemId) {
             R.id.nav_settings -> {
                 Snackbar.make(turn_on_adb_wifi, "TODO", Snackbar.LENGTH_SHORT).show()
@@ -71,6 +79,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun displaySelectedScreen(itemId: Int) {
+        when (itemId) {
+            R.id.nav_settings -> fragment = Settings()
+            R.id.nav_about -> fragment = About()
+        }
+
+        //replacing the fragment
+        if (fragment != null) {
+            val ft = supportFragmentManager.beginTransaction()
+            ft.replace(R.id.container, fragment)
+            ft.addToBackStack(null).commit()
+        }
+
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
     }
 
     private fun changeState() {
@@ -128,11 +154,29 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    private fun checkWiFi(): Boolean {
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        if (activeNetwork != null) { // connected to the internet
+            if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
+                // connected to wifi
+                //Snackbar.make(turn_on_adb_wifi, activeNetwork.typeName, Snackbar.LENGTH_SHORT).show()
+                return true
+            } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
+                // connected to the mobile provider's data plan
+                //Snackbar.make(turn_on_adb_wifi, activeNetwork.typeName, Snackbar.LENGTH_SHORT).show()
+                return false
+            }
+        } else {
+            // not connected to the internet
+            return false
+        }
+        return false
+    }
     private fun getIP(): String {
         val wifiMgr = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val wifiInfo = wifiMgr.connectionInfo
         val ip = wifiInfo.ipAddress
-        val strip = String.format("%d.%d.%d.%d", ip and 0xff, ip shr 8 and 0xff, ip shr 16 and 0xff, ip shr 24 and 0xff)
-        return strip
+        return String.format("%d.%d.%d.%d", ip and 0xff, ip shr 8 and 0xff, ip shr 16 and 0xff, ip shr 24 and 0xff)
     }
 }
